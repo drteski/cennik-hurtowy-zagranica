2ba392c20e9cba7c536c717047807821a1905f3c
Krzysztof Tomaszewski <drteski@gmail.com>
2024-09-30 21:44:47 +0200

    init

--- services/feed/feed.php
+++ services/feed/feed.php
@@ -1,6 +1,6 @@
 <?php
-# $url = 'https://files.lazienka-rea.com.pl/feed_3.xml';
-$url = 'https://lazienka-rea.com.pl/feed/generate/full_offer';
+$url = 'https://files.lazienka-rea.com.pl/feed_3.xml';
+# $url = 'https://lazienka-rea.com.pl/feed/generate/full_offer';
 $xmlDownloadLocation = '../../public/temp/';
 $jsonSaveLocation = '../../public/temp/data/';
 $productsPerFile = 500; // Updated batch size to 500
@@ -8,67 +8,79 @@ $productsPerFile = 500; // Updated batch size to 500
 function xmlToArray($xml, $options = array())
 {
     $defaults = array(
-        'namespaceSeparator' => ':',  // to distinguish namespaces
-        'attributePrefix' => '$',     // prefix for attributes
-        'alwaysArray' => array(),     // tags that should always be arrays
-        'autoArray' => true,          // create arrays for tags appearing more than once
-        'textContent' => '$',         // key used for text content
-        'autoText' => true,           // skip textContent key if no attributes or child nodes
-        'keySearch' => false,         // optional search and replace on tag/attribute names
-        'keyReplace' => false         // replace values for keySearch
+        'namespaceSeparator' => ':',//you may want this to be something other than a colon
+        'attributePrefix' => '$',   //to distinguish between attributes and nodes with the same name
+        'alwaysArray' => array(),   //array of xml tag names which should always become arrays
+        'autoArray' => true,        //only create arrays for tags which appear more than once
+        'textContent' => '$',       //key used for the text content of elements
+        'autoText' => true,         //skip textContent key if node has no attributes or child nodes
+        'keySearch' => false,       //optional search and replace on tag and attribute names
+        'keyReplace' => false       //replace values for above search values (as passed to str_replace())
     );
     $options = array_merge($defaults, $options);
-
-    // Handle attributes
-    $attributesArray = [];
     $namespaces = $xml->getDocNamespaces();
-    $namespaces[''] = null; // Add base (empty) namespace
+    $namespaces[''] = null; //add base (empty) namespace
 
+    //get attributes from all namespaces
+    $attributesArray = array();
     foreach ($namespaces as $prefix => $namespace) {
         foreach ($xml->attributes($namespace) as $attributeName => $attribute) {
-            if ($options['keySearch']) {
-                $attributeName = str_replace($options['keySearch'], $options['keyReplace'], $attributeName);
-            }
-            $attributeKey = $options['attributePrefix'] . ($prefix ? $prefix . $options['namespaceSeparator'] : '') . $attributeName;
+            //replace characters in attribute name
+            if ($options['keySearch']) $attributeName =
+                str_replace($options['keySearch'], $options['keyReplace'], $attributeName);
+            $attributeKey = $options['attributePrefix']
+                . ($prefix ? $prefix . $options['namespaceSeparator'] : '')
+                . $attributeName;
             $attributesArray[$attributeKey] = (string)$attribute;
         }
     }
 
-    // Handle children
-    $tagsArray = [];
+    //get child nodes from all namespaces
+    $tagsArray = array();
     foreach ($namespaces as $prefix => $namespace) {
         foreach ($xml->children($namespace) as $childXml) {
+            //recurse into child nodes
             $childArray = xmlToArray($childXml, $options);
-            $childTagName = key($childArray);
-            $childProperties = current($childArray);
+            list($childTagName, $childProperties) = each($childArray);
 
-            if ($options['keySearch']) {
-                $childTagName = str_replace($options['keySearch'], $options['keyReplace'], $childTagName);
-            }
-            if ($prefix) {
-                $childTagName = $prefix . $options['namespaceSeparator'] . $childTagName;
-            }
+            //replace characters in tag name
+            if ($options['keySearch']) $childTagName =
+                str_replace($options['keySearch'], $options['keyReplace'], $childTagName);
+            //add namespace prefix, if any
+            if ($prefix) $childTagName = $prefix . $options['namespaceSeparator'] . $childTagName;
 
             if (!isset($tagsArray[$childTagName])) {
-                $tagsArray[$childTagName] = in_array($childTagName, $options['alwaysArray']) || !$options['autoArray']
-                    ? array($childProperties) : $childProperties;
-            } elseif (is_array($tagsArray[$childTagName]) && array_keys($tagsArray[$childTagName]) === range(0, count($tagsArray[$childTagName]) - 1)) {
+                //only entry with this key
+                //test if tags of this type should always be arrays, no matter the element count
+                $tagsArray[$childTagName] =
+                    in_array($childTagName, $options['alwaysArray']) || !$options['autoArray']
+                        ? array($childProperties) : $childProperties;
+            } elseif (
+                is_array($tagsArray[$childTagName]) && array_keys($tagsArray[$childTagName])
+                === range(0, count($tagsArray[$childTagName]) - 1)
+            ) {
+                //key already exists and is integer indexed array
                 $tagsArray[$childTagName][] = $childProperties;
             } else {
+                //key exists so convert to integer indexed array with previous value in position 0
                 $tagsArray[$childTagName] = array($tagsArray[$childTagName], $childProperties);
             }
         }
     }
 
-    $textContentArray = [];
+    //get text content of node
+    $textContentArray = array();
     $plainText = trim((string)$xml);
     if ($plainText !== '') $textContentArray[$options['textContent']] = $plainText;
 
+    //stick it all together
     $propertiesArray = !$options['autoText'] || $attributesArray || $tagsArray || ($plainText === '')
-        ? array_merge($attributesArray, $tagsArray, $textContentArray)
-        : $plainText;
+        ? array_merge($attributesArray, $tagsArray, $textContentArray) : $plainText;
 
-    return array($xml->getName() => $propertiesArray);
+    //return node as array
+    return array(
+        $xml->getName() => $propertiesArray
+    );
 }
 
 function fetchXml($url)
