030a7a86d40de2a681366d897d077ee2e0d7d4b2
Krzysztof Tomaszewski <drteski@gmail.com>
2024-10-14 09:26:14 +0200

    init

--- app/api/populate/route.js
+++ app/api/populate/route.js
@@ -6,20 +6,28 @@ export const GET = async () => {
   const data = config.data;
   const translations = config.translations;
   await Promise.all(
-    data.map(async (item) => {
-      const translationIndex = translations.findIndex(
-        (translation) => translation.iso === item.iso,
-      );
-      return prisma.country.create({
-        data: {
-          iso: item.iso,
-          name: item.name,
-          currency: item.currency,
-          locale: item.locale,
-          subject: translations[translationIndex].subject,
-        },
-      });
-    }),
+    data
+      .map(async (item) => {
+        const translationIndex = translations.findIndex(
+          (translation) => translation.iso === item.iso,
+        );
+        const existingCountry = await prisma.country.findFirst({
+          where: {
+            iso: item.iso,
+          },
+        });
+        if (!existingCountry)
+          return prisma.country.create({
+            data: {
+              iso: item.iso,
+              name: item.name,
+              currency: item.currency,
+              locale: item.locale,
+              subject: translations[translationIndex].subject,
+            },
+          });
+      })
+      .filter(Boolean),
   );
   return new NextResponse(JSON.stringify({ message: "gotowe" }), {
     status: 200,
